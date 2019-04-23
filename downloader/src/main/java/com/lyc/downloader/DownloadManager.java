d7879332422055c24d3fbc56b8f56c82c9060950
liuyuchuan <kevinliu.sir@qq.com>
2019-04-23 21:18:11 +0800

    add listeners

--- downloader/src/main/java/com/lyc/downloader/DownloadManager.java
+++ downloader/src/main/java/com/lyc/downloader/DownloadManager.java
@@ -17,11 +17,14 @@ import com.lyc.downloader.db.DownloadItemState;
 import okhttp3.OkHttpClient;
 
 import java.io.File;
+import java.lang.ref.WeakReference;
 import java.util.ArrayDeque;
 import java.util.ArrayList;
 import java.util.Deque;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * @author liuyuchuan
@@ -44,7 +47,10 @@ public class DownloadManager implements DownloadListener {
     private final Deque<Long> errorTasksId = new ArrayDeque<>();
     private final Deque<Long> pausingTasksId = new ArrayDeque<>();
     private final Deque<Long> finishedTasksId = new ArrayDeque<>();
-    private int maxRunnningTask = 4;
+    // avoid memory leak
+    private WeakReference<DownloadListener> userDownloadListener;
+    private WeakReference<RecoverListener> userRecoverListener;
+    private int maxRunningTask = 4;
 
     public DownloadManager(OkHttpClient client, Context appContext) {
         this.client = client;
@@ -109,24 +115,20 @@ public class DownloadManager implements DownloadListener {
                                 break;
                         }
                     }
+                    schedule();
                 });
             }
+            RecoverListener recoverListener;
+            if (!downloadInfoList.isEmpty() && (recoverListener = userRecoverListener.get()) != null) {
+                recoverListener.recoverReady(downloadInfoList);
+            }
         });
-    }
-
-    public int getMaxRunnningTask() {
-        return maxRunnningTask;
-    }
-
-    public void setMaxRunnningTask(int maxRunningTask) {
-        if (maxRunningTask < 0) return;
-        this.maxRunnningTask = maxRunningTask;
     }
 
     // first come first service
     @MainThread
     private void schedule() {
-        while (runningTasksId.size() < maxRunnningTask && !waitingTasksId.isEmpty()) {
+        while (runningTasksId.size() < maxRunningTask && !waitingTasksId.isEmpty()) {
             Long id = waitingTasksId.pollFirst();
             DownloadTask downloadTask = taskTable.get(id);
             if (downloadTask == null) {
@@ -140,17 +142,32 @@ public class DownloadManager implements DownloadListener {
 
     @Override
     public void onPrepared(long id) {
-
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onPrepared(id);
+            }
+        }
     }
 
     @Override
     public void onProgressUpdate(long id, long total, long cur) {
-
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onProgressUpdate(id, total, cur);
+            }
+        }
     }
 
     @Override
     public void onSpeedChange(long id, double bps) {
-
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onSpeedChange(id, bps);
+            }
+        }
     }
 
     @Override
@@ -163,16 +180,33 @@ public class DownloadManager implements DownloadListener {
             }
             schedule();
         });
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onPrepared(id);
+            }
+        }
     }
 
     @Override
     public void onDownloadStart(long id) {
-        // not use for now
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onDownloadStart(id);
+            }
+        }
     }
 
     @Override
     public void onDownloadPausing(long id) {
         onDownloadPaused(id);
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onDownloadPaused(id);
+            }
+        }
     }
 
     @Override
@@ -185,11 +219,23 @@ public class DownloadManager implements DownloadListener {
             }
             schedule();
         });
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onDownloadPaused(id);
+            }
+        }
     }
 
     @Override
     public void onDownloadCancelling(long id) {
         onDownloadCanceled(id);
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onDownloadCancelling(id);
+            }
+        }
     }
 
     @Override
@@ -204,6 +250,12 @@ public class DownloadManager implements DownloadListener {
             }
             schedule();
         });
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onDownloadCanceled(id);
+            }
+        }
     }
 
     @Override
@@ -216,6 +268,12 @@ public class DownloadManager implements DownloadListener {
             }
             schedule();
         });
+        if (userDownloadListener != null) {
+            DownloadListener downloadListener = userDownloadListener.get();
+            if (downloadListener != null) {
+                downloadListener.onDownloadFinished(id);
+            }
+        }
     }
 
     @WorkerThread
@@ -295,9 +353,70 @@ public class DownloadManager implements DownloadListener {
         DownloadExecutors.io.execute(() -> submitInner(url, path, headers, listener, downloadListener));
     }
 
+    @MainThread
+    public int getMaxRunningTask() {
+        return maxRunningTask;
+    }
+
+    @MainThread
+    public void setMaxRunningTask(int maxRunningTask) {
+        if (maxRunningTask < 0) return;
+        this.maxRunningTask = maxRunningTask;
+    }
+
+    @MainThread
+    public void setUserDownloadListener(DownloadListener downloadListener) {
+        if (userDownloadListener != null) {
+            userDownloadListener.clear();
+        }
+        userDownloadListener = new WeakReference<>(downloadListener);
+    }
+
+    @MainThread
+    public void setUserRecoverListener(RecoverListener recoverListener) {
+        if (userRecoverListener != null) {
+            userRecoverListener.clear();
+        }
+        userRecoverListener = new WeakReference<>(recoverListener);
+    }
+
+    public DownloadInfo queryDownloadInfo(long id) {
+        return infoTable.get(id);
+    }
+
+    public List<DownloadInfo> queryAllDownloadInfo() {
+        List<DownloadInfo> result = new ArrayList<>();
+        Set<Long> addIds = new HashSet<>();
+        for (Long aLong : runningTasksId) {
+            if (addIds.add(aLong)) {
+                result.add(infoTable.get(aLong));
+            }
+        }
+        for (Long aLong : waitingTasksId) {
+            if (addIds.add(aLong)) {
+                result.add(infoTable.get(aLong));
+            }
+        }
+        for (Long aLong : pausingTasksId) {
+            if (addIds.add(aLong)) {
+                result.add(infoTable.get(aLong));
+            }
+        }
+        for (Long aLong : errorTasksId) {
+            if (addIds.add(aLong)) {
+                result.add(infoTable.get(aLong));
+            }
+        }
+        return result;
+    }
+
     public interface SubmitListener {
         void submitSuccess(long id);
 
         void submitFail(Exception e);
+    }
+
+    public interface RecoverListener {
+        void recoverReady(List<DownloadInfo> recoveredTasks);
     }
 }
